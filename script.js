2f67969113148eb9e58c9289799fc0ef0f4383ce
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-20 02:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ function insert_Row() {
 	let tr = document.createElement('tr');
 	let td1 = document.createElement('td');
 	let td2 = document.createElement('td');
-	td1.innerHTML = "New Cell1";
-	td2.innerHTML = "New Cell2";
+	td1.innerHTML += "New Cell1";
+	td2.innerHTML += "New Cell2";
+	console.log(td1);
+	console.log(td2);
 	tr.appendChild(td1);
 	tr.appendChild(td2);
 	table.prepend(tr);
