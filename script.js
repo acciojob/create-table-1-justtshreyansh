f85f6134b3d4a6a3013aafe73057e5547e617bcd
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-20 02:28:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+let table = document.querySelector("#sampleTable");
 function insert_Row() {
     //Write your code here
+	let tr = document.createElement('tr');
+	let td1 = document.createElement('td');
+	let td2 = document.createElement('td');
+	td1.textContent = "New Cell1";
+	td2.textContent = "New Cell2";
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	table.prepend(tr);
+	
   
   
 }
